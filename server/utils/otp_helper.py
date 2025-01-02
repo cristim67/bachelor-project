b5b8d3208f7fedaaa650a8a3a54b63b2486cd3b5
Cristi Miloiu <miloiuc4@gmail.com>
2025-01-02 19:48:35 +0200

    Add backend pre-commit unit tests

--- server/utils/otp_helper.py
+++ server/utils/otp_helper.py
@@ -1,9 +1,9 @@
 import random
 import string
-from config.env_handler import OTP_EXPIRATION_MINUTES
+from config.env_handler import OTP_CODE_LENGTH
 
 def generate_otp_code():
-    return ''.join(random.choices(string.digits, k=OTP_EXPIRATION_MINUTES))
+    return ''.join(random.choices(string.digits, k=OTP_CODE_LENGTH))
 
 def is_otp_code_valid(otp_code: str):
-    return otp_code in string.digits and len(otp_code) == OTP_EXPIRATION_MINUTES
+    return len(otp_code) == OTP_CODE_LENGTH
