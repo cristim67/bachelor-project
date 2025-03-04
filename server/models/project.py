f2e87ea41882f785ffc65e56fae6854ba4a50058
Cristi Miloiu <miloiuc4@gmail.com>
2025-03-04 22:27:24 +0200

    fix dtos

--- server/models/project.py
+++ server/models/project.py
@@ -2,14 +2,15 @@ from datetime import datetime
 from typing import Optional
 
 from beanie import Document
+from dtos.project import Stack
 
 
 class Project(Document):
     idea: str
-    stack: dict
+    stack: Stack
     user_id: str
     is_public: bool = True
-    s3_folder_name: str = None
+    s3_folder_name: Optional[str] = None
     s3_presigned_url: Optional[str] = None
     created_at: datetime = datetime.now()
     updated_at: datetime = datetime.now()
