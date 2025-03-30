5a2d5723f6dc0fef03e7d2eff56739b4115918cd
Cristi Miloiu <miloiuc4@gmail.com>
2025-03-30 14:52:22 +0300

    update logger to print stack trace by default

--- server/config/logger.py
+++ server/config/logger.py
@@ -1,23 +1,53 @@
 import sys
+import traceback
 
-from config.env_handler import ENVIRONMENT
 from loguru import logger
 
-LOG_LEVEL = "DEBUG" if ENVIRONMENT == "dev" else "INFO"
+LOG_LEVEL = "INFO"
 
 logger.remove()
-logger.add(
-    sys.stdout,
-    format="<level>{level}</level> | <green>{time:YYYY-MM-DD HH:mm:ss}</green> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | {message}",
-    level=LOG_LEVEL,
-    colorize=True,
-)
 
-logger.add(
-    sys.stderr,
-    format="<level>{level}</level> | <green>{time:YYYY-MM-DD HH:mm:ss}</green> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>\n{message}",
-    level="ERROR",
-    colorize=True,
-    backtrace=True,
-    diagnose=True,
-)
+
+def format_log(record):
+    return f"{record['level']} | {record['time'].strftime('%Y-%m-%d %H:%M:%S')} | {record['name']}:{record['function']}:{record['line']} | {record['message']}"
+
+
+def info_handler(message):
+    print(format_log(message.record))
+
+
+def error_handler(message):
+    error_text = (
+        f"{message.record['level']} | {message.record['time'].strftime('%Y-%m-%d %H:%M:%S')} | "
+        f"{message.record['name']}:{message.record['function']}:{message.record['line']}\n"
+        f"⚠️ Error: {message.record['message']}\n"
+    )
+
+    exception_data = message.record["exception"]
+    if exception_data:
+        if isinstance(exception_data, tuple) and len(exception_data) == 3:
+            exc_type, exc_value, exc_tb = exception_data
+            formatted_tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+            error_text += f"Stack trace:\n{formatted_tb}"
+        else:
+            error_text += f"Stack trace:\n{exception_data}"
+
+    print(error_text)
+
+
+logger.add(info_handler, level="INFO", filter=lambda record: record["level"].name == "INFO")
+logger.add(error_handler, level="ERROR", backtrace=True, diagnose=True)
+
+original_error = logger.error
+
+
+def new_error(message, *args, **kwargs):
+    exc_info = sys.exc_info()
+
+    if exc_info[0] is not None:
+        logger.opt(exception=exc_info).error(message, *args, **kwargs)
+    else:
+        original_error(message, *args, **kwargs)
+
+
+logger.error = new_error
