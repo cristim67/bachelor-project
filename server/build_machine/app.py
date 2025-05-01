ff6151665ba328754676de674935715e88b565c3
Cristi Miloiu <miloiuc4@gmail.com>
2025-05-01 14:55:17 +0300

    feat: deployment flow

--- server/build_machine/app.py
+++ server/build_machine/app.py
@@ -1,17 +1,21 @@
 import io
+import os
 import subprocess
 import tempfile
+import uuid
 import zipfile
-from typing import Optional
 
 import aiohttp
 from dotenv import load_dotenv
 from dtos.project import ProjectData
-from fastapi import Depends, FastAPI, Request, status
+from fastapi import Depends, FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBearer
 
 load_dotenv()
+
+token = os.getenv("GENEZIO_TOKEN")
 
 security = HTTPBearer()
 
@@ -29,22 +33,35 @@ app = FastAPI(
     },
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/health")
 async def health():
     return JSONResponse(status_code=status.HTTP_200_OK, content={"status": "ok"})
 
-@app.get("/test-genezio")
-async def test_genezio():
-    result = subprocess.run(["genezio", "login"], capture_output=True, text=True)
+@app.get("/genezio-login")
+async def genezio_login():
+    print("Running genezio login...")
+    result = subprocess.run(["genezio", "login", token], capture_output=True, text=True, env={"CI": "true", **os.environ})
+    if result.stderr:
+        print("Login errors:", result.stderr)
+    result = subprocess.run(["genezio", "account"], capture_output=True, text=True, env={"CI": "true", **os.environ})
+    if result.stderr:
+        print("Account errors:", result.stderr)
     return JSONResponse(status_code=status.HTTP_200_OK, content={"status": "ok", "result": result.stdout})
 
 @app.post("/project-build")
-async def project_build(request: ProjectData, token: str = Depends(security)):
+async def project_build(request: ProjectData, credentials: HTTPBearer = Depends(security)):
     presigned_url = request.presigned_url
-    genezio_token = request.genezio_token
-
+    print("Presigned URL:", presigned_url)
     try:
-        temp_dir = tempfile.mkdtemp()
+        temp_dir = tempfile.mkdtemp() + str(uuid.uuid4())
         # download the project from the presigned url
         async with aiohttp.ClientSession() as session:
             async with session.get(presigned_url) as response:
@@ -56,22 +73,41 @@ async def project_build(request: ProjectData, token: str = Depends(security)):
         with zipfile.ZipFile(io.BytesIO(project_zip), "r") as zip_ref:
             zip_ref.extractall(temp_dir)
 
+        # unzip the code.zip file
+        code_zip_path = os.path.join(temp_dir, "code", "code.zip")
+        with zipfile.ZipFile(code_zip_path, "r") as code_zip_ref:
+            code_zip_ref.extractall(os.path.join(temp_dir, "code"))
+
+        # print the project directory tree
+        print("Project directory tree after unzip:")
+        tree_result = subprocess.run(["tree", temp_dir, "-L", "2"], capture_output=True, text=True, env={"CI": "true", **os.environ})
+        print(tree_result.stdout)
+        if tree_result.stderr:
+            print("Tree errors:", tree_result.stderr)
+        
         # genezio analyze 
         print("Running genezio analyze...")
         analyze_result = subprocess.run(["genezio", "analyze"], 
                                      capture_output=True, 
                                      text=True,
-                                     cwd=temp_dir)
+                                     cwd= os.path.join(temp_dir, "code"),
+                                     env={"CI": "true", **os.environ})
         print("Analyze output:", analyze_result.stdout)
+
+        print("Cat genezio.yaml file:")
+        with open(os.path.join(temp_dir, "code", "genezio.yaml"), "r") as f:
+            print(f.read())
         if analyze_result.stderr:
             print("Analyze errors:", analyze_result.stderr)
 
         # genezio login
         print("Running genezio login...")
-        login_result = subprocess.run(["genezio", "login", genezio_token], 
-                                    capture_output=True, 
-                                    text=True,
-                                    env={"CI": "true"})
+        login_result = subprocess.run(
+            ["genezio", "login", token],
+            capture_output=True,
+            text=True,
+            env={"CI": "true", **os.environ}
+        )
         print("Login output:", login_result.stdout)
         if login_result.stderr:
             print("Login errors:", login_result.stderr)
@@ -81,7 +117,8 @@ async def project_build(request: ProjectData, token: str = Depends(security)):
         deploy_result = subprocess.run(["genezio", "deploy"], 
                                      capture_output=True, 
                                      text=True,
-                                     cwd=temp_dir)
+                                     cwd= os.path.join(temp_dir, "code"),
+                                     env={"CI": "true", **os.environ})
         print("Deploy output:", deploy_result.stdout)
         if deploy_result.stderr:
             print("Deploy errors:", deploy_result.stderr)
--- server/build_machine/dtos/project.py
+++ server/build_machine/dtos/project.py
@@ -2,5 +2,4 @@ from pydantic import BaseModel, Field
 
 
 class ProjectData(BaseModel):
-    genezio_token: str = Field(..., description="The genezio token")
     presigned_url: str = Field(..., description="The presigned URL of the project")
